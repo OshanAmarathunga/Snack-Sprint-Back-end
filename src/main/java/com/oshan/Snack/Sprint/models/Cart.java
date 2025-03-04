953bddc587eb289c461b164e7f385434f7d15bdc
oshanamarathunga <oshanamarathunga@gmail.com>
2025-03-04 16:23:27 +0530

    Almost completed entity classes

--- src/main/java/com/oshan/Snack/Sprint/models/Cart.java
+++ src/main/java/com/oshan/Snack/Sprint/models/Cart.java
@@ -1,26 +1,24 @@
 package com.oshan.Snack.Sprint.models;
 
-import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Entity
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
-public class IngredientsItem {
+public class Cart {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
-
-    private String name;
-    @ManyToOne
-    private IngredientCategory category;
-    @JsonIgnore
-    @ManyToOne
-    private Restaurant restaurant;
-    private boolean isStock=true;
-
+    @OneToOne
+    private User customer;
+    private Long total;
+    @OneToMany(mappedBy = "cart",cascade = CascadeType.ALL,orphanRemoval = true)
+    private List<CartItem> item=new ArrayList<>();
 }
