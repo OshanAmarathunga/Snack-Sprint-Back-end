e0e89bb77275c117177623872005249a5843c2c6
oshanamarathunga <oshanamarathunga@gmail.com>
2025-03-05 09:15:11 +0530

    added jwtProvider

---
+++ src/main/java/com/oshan/snack/sprint/controller/AuthController.java
@@ -0,0 +1,14 @@
+package com.oshan.snack.sprint.controller;
+
+import com.oshan.snack.sprint.repository.UserRepository;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/auth")
+public class AuthController {
+    private UserRepository userRepository;
+    private PasswordEncoder passwordEncoder;
+
+}
