400fdb6ac088a684f27667a4d28a5c7be67b6d70
oshanamarathunga <oshanamarathunga@gmail.com>
2025-03-06 11:35:08 +0530

    completed admin restaurant controller

--- src/main/java/com/oshan/snack/sprint/controller/AdminRestaurantController.java
+++ src/main/java/com/oshan/snack/sprint/controller/AdminRestaurantController.java
@@ -51,6 +51,29 @@ public class AdminRestaurantController {
         restaurantService.deleteRestaurant(id);
         MessageResponse res=new MessageResponse();
         res.setMessage("Restaurant Deleted success!");
-        return new ResponseEntity<>(res, HttpStatus.CREATED);
+        return new ResponseEntity<>(res, HttpStatus.OK);
+    }
+
+    @PutMapping("/{id}/status")
+    public ResponseEntity<Restaurant> updateRestaurantStatus(
+            @RequestBody CreateRestaurantRequest req,
+            @RequestHeader("Authorization") String jwt,
+            @PathVariable Long id
+    ) throws Exception {
+        User user=userService.findUserByJwtToken(jwt);
+        Restaurant restaurant = restaurantService.updateRestaurantStatus(id);
+
+        return new ResponseEntity<>(restaurant, HttpStatus.OK);
+    }
+
+    @GetMapping("/user")
+    public ResponseEntity<Restaurant> findRestaurantByUserId(
+            @RequestBody CreateRestaurantRequest req,
+            @RequestHeader("Authorization") String jwt
+    ) throws Exception {
+        User user=userService.findUserByJwtToken(jwt);
+        Restaurant restaurant = restaurantService.findRestaurantById(user.getId());
+
+        return new ResponseEntity<>(restaurant, HttpStatus.OK);
     }
 }
