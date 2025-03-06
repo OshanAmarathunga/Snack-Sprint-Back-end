12f5b752f51357b8906e94ee13822f431fda6db1
oshanamarathunga <oshanamarathunga@gmail.com>
2025-03-06 16:45:07 +0530

    created Food service

--- src/main/java/com/oshan/snack/sprint/service/FoodService.java
+++ src/main/java/com/oshan/snack/sprint/service/FoodService.java
@@ -3,9 +3,22 @@ package com.oshan.snack.sprint.service;
 import com.oshan.snack.sprint.models.Category;
 import com.oshan.snack.sprint.models.Food;
 import com.oshan.snack.sprint.models.Restaurant;
+import com.oshan.snack.sprint.request.CreateFoodRequest;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public interface FoodService {
     public Food createFood(CreateFoodRequest req, Category category, Restaurant restaurant);
+    void deleteFood(Long foodId) throws Exception;
+    public List<Food> getRestaurantsFood(Long restaurantId,
+                                         boolean isVegetarian,
+                                         boolean isNonveg,
+                                         boolean isSeasonal,
+                                         String foodCategory);
+
+    public List<Food> searchFood(String keyword);
+    public Food findFoodById(Long foodId) throws Exception;
+    public Food updateAvailabilityStatus(Long foodId) throws Exception;
 }
