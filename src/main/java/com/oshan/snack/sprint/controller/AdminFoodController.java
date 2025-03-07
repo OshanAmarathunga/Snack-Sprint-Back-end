c736b74ad25c32bb9551db353139d6d54ddca344
oshanamarathunga <oshanamarathunga@gmail.com>
2025-03-07 11:08:37 +0530

    new feature/created adminFoodontroller classes/ oshan

---
+++ src/main/java/com/oshan/snack/sprint/controller/AdminFoodController.java
@@ -0,0 +1,46 @@
+package com.oshan.snack.sprint.controller;
+
+import com.oshan.snack.sprint.models.Food;
+import com.oshan.snack.sprint.models.Restaurant;
+import com.oshan.snack.sprint.models.User;
+import com.oshan.snack.sprint.request.CreateFoodRequest;
+import com.oshan.snack.sprint.response.MessageResponse;
+import com.oshan.snack.sprint.service.FoodService;
+import com.oshan.snack.sprint.service.RestaurantService;
+import com.oshan.snack.sprint.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/admin/food")
+public class AdminFoodController {
+    @Autowired
+    private FoodService foodService;
+
+    @Autowired
+    private UserService userService;
+
+    @Autowired
+    private RestaurantService restaurantService;
+
+    @PostMapping()
+    public ResponseEntity<Food> createFood(@RequestBody CreateFoodRequest req,
+                                           @RequestHeader("Authorization") String jwt) throws Exception {
+        User user=userService.findUserByJwtToken(jwt);
+        Restaurant restaurant=restaurantService.findRestaurantById(req.getRestaurantId());
+        Food food=foodService.createFood(req,req.getCategory(),restaurant);
+        return new ResponseEntity<>(food, HttpStatus.CREATED);
+    }
+
+    @PostMapping("/{id}")
+    public ResponseEntity<MessageResponse> deleteFood(@PathVariable Long id,
+                                                      @RequestHeader("Authorization") String jwt) throws Exception {
+        User user=userService.findUserByJwtToken(jwt);
+        foodService.deleteFood(id);
+        MessageResponse res=new MessageResponse();
+        res.setMessage("Food deleted Sucess!");
+        return new ResponseEntity<>(res, HttpStatus.OK);
+    }
+}
