23a89ab1d9b49ca19b008f013cc60ff1638d4786
oshanamarathunga <oshanamarathunga@gmail.com>
2025-03-16 18:32:38 +0530

    New Feature/update order/ oshan

--- src/main/java/com/oshan/snack/sprint/service/impl/OrderServiceImpl.java
+++ src/main/java/com/oshan/snack/sprint/service/impl/OrderServiceImpl.java
@@ -74,7 +74,17 @@ public class OrderServiceImpl implements OrderService {
 
     @Override
     public Order updateOrder(Long orderId, String orderStatus) throws Exception {
-        return null;
+        Order order=findOrderById(orderId);
+        if (orderStatus.equals("OUT_FOR_DELIVERY")
+        || orderStatus.equals("DELIVERED")
+        || orderStatus.equals("COMPLETED")
+        || orderStatus.equals("PENDING")){
+            order.setOrderStatus(orderStatus);
+            return orderRepository.save(order);
+        }
+
+        throw new Exception("Please select a valid order status");
+
     }
 
     @Override
